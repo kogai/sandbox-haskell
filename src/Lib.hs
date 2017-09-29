21ca92a6442314aa6956aa1a24f3ae5183d05865
shinichi kogai <kogai0121@gmail.com>
2017-09-29 15:09:10 +0900

    Fib

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,14 @@
 module Lib
-    ( someFunc
+    ( someFunc,
+    fib
     ) where
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = putStrLn "Hello world"
+
+-- 0, 1, 2, 3, 4, 5, 6, 07, 08, 09...
+-- 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946...
+fib :: Int -> Int
+fib 0 = 0
+fib 1 = 1
+fib n = (fib $ n - 1) + (fib $ n - 2)
