2e7bed58781eb10d1e190dadac89a94099b55cf0
shinchi kogai <kogai0121@gmail.com>
2017-10-01 15:10:28 +0900

    trace

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,10 +1,13 @@
 module Lib (
-    someFunc  ,
-    fib
+    someFunc
+  , fib
+  , f
 ) where
 
+import           Debug.Trace
+
 someFunc :: IO ()
-someFunc = putStrLn "Hello world"
+someFunc = putStrLn    "Hello world"
 
 -- 0, 1, 2, 3, 4, 5, 6, 07, 08, 09...
 -- 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946...
@@ -14,4 +17,4 @@ fib 1 = 1
 fib n = fib (n - 1) + fib (n - 2)
 
 f :: Int -> String
-f n = n
+f n = trace ("function [f] called with [" ++ show n ++ "]!!!") $ show $ fib n
